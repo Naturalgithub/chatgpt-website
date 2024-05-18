ecc7244e49288abfe5569f7f0cdd75ac63ff6e78
yuanquanke <3318615021@qq.com>
2024-05-18 13:34:33 +0800

    🚀 feat:yuanquanke专用

--- src/utils/dict.ts
+++ src/utils/dict.ts
@@ -15,4 +15,8 @@ export const USER_VIP = {
         dest: '梅春',
         key: 'sk-KGICSIFn738e98147507T3BLbKFJ1Bff87B00ffE4fAaB31B'
     },
+    "yuanquanke": {
+        dest: 'yuanquanke',
+        key: 'sk-HhKDtCDM456FF1df8E94T3BLBkFJd26BA4e8480E43F591f8'
+    }
 }
