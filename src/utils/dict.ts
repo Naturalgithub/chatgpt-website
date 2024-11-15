bf4b8a8fb3bd9a8f7289d3c79edd1d907f278c1c
yuanquanke <3318615021@qq.com>
2024-11-15 17:11:53 +0800

    🐞 fix: 升级密钥

--- src/utils/dict.ts
+++ src/utils/dict.ts
@@ -1,22 +1,6 @@
 export const USER_VIP = {
-    "maliu": {
-        dest: '马骝通用',
-        key: 'sk-KGICSIFn738e98147507T3BLbKFJ1Bff87B00ffE4fAaB31B'
-    },
-    "kexiaoxiao": {
-        dest: '马骝通用2',
-        key: 'sk-KGICSIFn738e98147507T3BLbKFJ1Bff87B00ffE4fAaB31B'
-    },
-    "temp": {
-        dest: '临时密钥',
-        key: 'sk-FGONbWZy89E5844FC0e5T3BLBkFJeD79Ff5b82f5404291F8'
-    },
-    "meichun": {
-        dest: '梅春',
-        key: 'sk-KGICSIFn738e98147507T3BLbKFJ1Bff87B00ffE4fAaB31B'
-    },
     "yuanquanke": {
         dest: 'yuanquanke',
-        key: 'sk-HhKDtCDM456FF1df8E94T3BLBkFJd26BA4e8480E43F591f8'
+        key: 'sk-KGICSIFn738e98147507T3BLbKFJ1Bff87B00ffE4fAaB31B'
     }
 }
